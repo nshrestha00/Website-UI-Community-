fd0adda54d3503629d70f14ba9dbc8ce36e0d79d
Your Name <your.email@example.com>
2025-03-09 18:50:35 +0545

    Insert images in services of fitenss website

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,23 +2,27 @@ import fitness_logo from './fitness_logo.png';
 import p1 from './p1.png';
 import p2 from './p2.png';
 import wholebodyfatloss from './wholebodyfatloss.png';
-// import zumba from './zumba.png';
+import zumba from './zumba.png';
 import yoga from './yoga.png';
 import bodybuilding from './bodybuilding.png';
-// import aerobics from './aerobics.png';
-// import freeWeights from './freeWeights.png';
+import aerobics from './aerobics.png';
+import freeWeights from './freeWeights.png';
+import before from './before.png';
+import after from './after.png';
 
 export const assets = {
   fitness_logo,
   p1,
   p2,
+  before,
+  after,
 };
 
 export const services = [
   { name: "Whole body fat loss", icon: wholebodyfatloss },
-  // { name: "Zumba", icon: zumba },
+  { name: "Zumba", icon: zumba },
   { name: "Yoga", icon: yoga },
   { name: "Bodybuilding", icon: bodybuilding },
-  // { name: "Aerobics", icon: aerobics },
-  // { name: "Free Weights", icon: freeWeights },
+  { name: "Aerobics", icon: aerobics },
+  { name: "Free Weights", icon: freeWeights },
 ];
--- src/websites/Fitness/Review.jsx
+++ src/websites/Fitness/Review.jsx
@@ -1,5 +1,9 @@
+
+
 const Review = () => {
-  return <div>Review</div>
+  return (
+    <div>Review</div>
+  )
 }
 
 export default Review
