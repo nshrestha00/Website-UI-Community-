eb3df7ee8c31c8c4b1d4c4936b889f34168fc65c
Your Name <your.email@example.com>
2025-03-11 19:31:13 +0545

    Fixed the navbar typo mistake

--- src/websites/Coffee/Navbar.jsx
+++ src/websites/Coffee/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="text-black p-4 flex justify-between items-center sticky top-0 z-40">
       <img src={assets.coffee_logo} alt="coffee Logo" className="h-10" />
       <div className="space-x-4">
-        <a href="#coffee-home" className="cursor-pointer">Home</a>
+        <a href="#coffee-about" className="cursor-pointer">About</a>
         <a href="#coffee-services" className="cursor-pointer">Services</a>
         <a href="#coffee-findus" className="cursor-pointer">FindUs</a>
         <a href="#coffee-menu" className="cursor-pointer">Menu</a>
