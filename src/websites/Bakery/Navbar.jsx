3771691f2bc1ad42c94630c9c62696663310ca21
Your Name <your.email@example.com>
2025-03-14 20:04:02 +0545

    Added active link in bakery  navbar

--- src/websites/Bakery/Navbar.jsx
+++ src/websites/Bakery/Navbar.jsx
@@ -1,13 +1,43 @@
+import { useState } from 'react';
 import { assets } from '../../assets/assets';
 
 const Navbar = () => {
+  const [activeLink, setActiveLink] = useState("");
+  
+    const handleClick = (link) => {
+      setActiveLink(link);
+    };
+  
+    const linkClasses = (link) =>
+      `cursor-pointer ${
+        activeLink === link ? "font-bold underline" : ""
+      }`;
+  
   return (
     <nav className="text-black p-4 flex justify-between items-center sticky top-0 z-40">
       <img src={assets.bakery_logo} alt="Bakery Logo" className="h-10" />
       <div className="space-x-4">
-        <a href="#bakery-home" className="cursor-pointer">Home</a>
-        <a href="#bakery-menu" className="cursor-pointer">Menu</a>
-        <a href="#bakery-about" className="cursor-pointer">About</a>
+        <a 
+          href="#bakery-home"  
+          className={linkClasses("home")}
+          onClick={() => handleClick("home")}
+        >
+          Home
+        </a>
+        <a 
+          href="#bakery-menu"  
+          className={linkClasses("menu")}
+          onClick={() => handleClick("menu")}
+          >
+            Menu
+        </a>
+        <a 
+          href="#bakery-about" 
+           className={linkClasses("about")}
+          onClick={() => handleClick("about")}
+          >
+            About
+        </a>
       </div>
     </nav>
   );
