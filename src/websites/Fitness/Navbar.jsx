88307b9ed58534aad78a4154db15984b0f26daa4
Your Name <your.email@example.com>
2025-03-14 20:11:29 +0545

    Added active link in fitness navbar

--- src/websites/Fitness/Navbar.jsx
+++ src/websites/Fitness/Navbar.jsx
@@ -1,15 +1,57 @@
+import { useState } from 'react';
 import { assets } from '../../assets/assets';
 
 const Navbar = () => {
+  const [activeLink, setActiveLink] = useState("");
+  
+    const handleClick = (link) => {
+      setActiveLink(link);
+    };
+  
+    const linkClasses = (link) =>
+      `cursor-pointer ${
+        activeLink === link ? "font-bold underline" : ""
+      }`;
+  
   return (
     <nav className="bg-black text-white p-4 flex justify-between items-center sticky top-0 z-40">
       <img src={assets.fitness_logo} alt="Fitness Logo" className="h-10" />
       <div className="space-x-4">
-        <a href="#fitness-home" className="cursor-pointer">Home</a>
-        <a href="#fitness-about" className="cursor-pointer">About</a>
-        <a href="#fitness-review" className="cursor-pointer">Review</a>
-        <a href="#fitness-services" className="cursor-pointer">Services</a>
-        <a href="#fitness-contact" className="cursor-pointer">Contact</a>
+        <a 
+          href="#fitness-home" 
+           className={linkClasses("home")}
+          onClick={() => handleClick("home")}
+          >
+          Home
+        </a>
+        <a 
+          href="#fitness-about" 
+           className={linkClasses("about")}
+          onClick={() => handleClick("about")}
+          >
+            About
+        </a>
+        <a 
+          href="#fitness-review" 
+           className={linkClasses("review")}
+          onClick={() => handleClick("review")}
+          >
+            Review
+        </a>
+        <a 
+          href="#fitness-services" 
+           className={linkClasses("services")}
+          onClick={() => handleClick("services")}
+          >
+            Services
+        </a>
+        <a 
+          href="#fitness-contact" 
+           className={linkClasses("contact")}
+          onClick={() => handleClick("contact")}
+          >
+            Contact
+        </a>
       </div>
     </nav>
   );
